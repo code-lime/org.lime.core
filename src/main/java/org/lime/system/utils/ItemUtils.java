dd94ebeb837ef91437575fe3814320e8f7d04a94
Lime <demn2001@gmail.com>
2023-09-17 20:13:10 +0300

    save `AIR` item to `null`

--- src/main/java/org/lime/system/utils/ItemUtils.java
+++ src/main/java/org/lime/system/utils/ItemUtils.java
@@ -1,12 +1,13 @@
 package org.lime.system.utils;
 
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Base64;
 
 public class ItemUtils {
     public static String saveItem(ItemStack item) {
-        return item == null ? null : new String(Base64.getEncoder().encode(item.serializeAsBytes()));
+        return item == null || item.getType() == Material.AIR ? null : new String(Base64.getEncoder().encode(item.serializeAsBytes()));
     }
     public static ItemStack loadItem(String str) {
         return str == null ? null : ItemStack.deserializeBytes(Base64.getDecoder().decode(str.getBytes()));
