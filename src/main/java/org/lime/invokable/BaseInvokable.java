5d89fcefbee6c339af0ce4b05c5c8637bd0a186e
Lime <demn2001@gmail.com>
2024-11-21 15:49:29 +0300

    refactor

--- src/main/java/org/lime/invokable/BaseInvokable.java
+++ src/main/java/org/lime/invokable/BaseInvokable.java
@@ -1,9 +1,9 @@
 package org.lime.invokable;
 
-public abstract class IInvokable {
+public abstract class BaseInvokable {
     public int waitTicks;
 
-    public IInvokable(int waitTicks) { this.waitTicks = waitTicks; }
+    public BaseInvokable(int waitTicks) { this.waitTicks = waitTicks; }
     public abstract void invoke() throws Throwable;
     public boolean tryRemoveInvoke() throws Throwable {
         if (--waitTicks > 0) return false;
