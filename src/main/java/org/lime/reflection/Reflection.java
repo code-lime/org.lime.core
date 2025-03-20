c7498b2829f6e7e30c9038bca1fa26e50db512ff
code-lime <demn2001@gmail.com>
2025-03-20 22:24:48 +0300

    extract global unsafe to `NMS` `Native`

--- src/main/java/org/lime/reflection/Reflection.java
+++ src/main/java/org/lime/reflection/Reflection.java
@@ -1,88 +1,24 @@
 package org.lime.reflection;
 
 import com.google.common.collect.Streams;
+import net.minecraft.unsafe.Native;
 import org.lime.system.execute.Func1;
 import org.lime.Unsafe;
 import org.objectweb.asm.Type;
 
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.VarHandle;
 import java.lang.reflect.*;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
 import java.util.*;
 import java.util.stream.Stream;
 
-@SuppressWarnings("removal")
 public class Reflection {
-    private static final VarHandle MODIFIERS;
-    private static final VarHandle SIGNATURE;
-    static {
-        try {
-            MODIFIERS = MethodHandles.privateLookupIn(Field.class, MethodHandles.lookup())
-                    .findVarHandle(Field.class, "modifiers", int.class);
-            SIGNATURE = MethodHandles.privateLookupIn(Method.class, MethodHandles.lookup())
-                    .findVarHandle(Method.class, "signature", String.class);
-        } catch (IllegalAccessException | NoSuchFieldException ex) {
-            throw new IllegalArgumentException(ex);
-        }
-    }
-    public static String signature(Method method){
-        String sig = (String)SIGNATURE.get(method);
-        if(sig != null) return sig;
-
-        StringBuilder sb = new StringBuilder("(");
-        for(Class<?> c : method.getParameterTypes())
-            sb.append((sig = Array.newInstance(c, 0).toString()), 1, sig.indexOf('@'));
-        return sb.append(')')
-                .append(method.getReturnType() == void.class
-                        ? "V"
-                        : (sig = Array.newInstance(method.getReturnType(), 0).toString()).substring(1, sig.indexOf('@'))
-                )
-                .toString()
-                .replace('.', '/');
+    public static String signature(Method method) {
+        return Native.signature(method);
     }
     public static Field nonFinal(Field field) {
-        int mods = field.getModifiers();
-        //if (Modifier.isFinal(mods)) {
-        try {
-            /*
-            Field modifiersField = Field.class.getDeclaredField("modifiers");
-            modifiersField.setAccessible(true);
-            modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-            */
-            Method getDeclaredFields0 = Class.class.getDeclaredMethod("getDeclaredFields0", boolean.class);
-            getDeclaredFields0.setAccessible(true);
-            Field[] fields = (Field[]) getDeclaredFields0.invoke(Field.class, false);
-            Field modifiers = null;
-            for (Field each : fields) {
-                if ("modifiers".equals(each.getName())) {
-                    modifiers = each;
-                    break;
-                }
-            }
-            assert modifiers != null;
-            modifiers.setAccessible(true);
-            mods &= ~Modifier.FINAL;
-            mods |= Modifier.PUBLIC;
-            modifiers.setInt(field, mods);
-            return field;
-        } catch (Exception e) {
-            throw new IllegalArgumentException(e);
-        }
-        //}
-        //if (Modifier.isFinal(mods)) MODIFIERS.set(field, mods & ~Modifier.FINAL);
-        //return field;
+        return Native.nonFinal(field);
     }
     public static <T extends AccessibleObject>T access(T val) {
-        if (System.getSecurityManager() == null) {
-            val.setAccessible(true);
-            return val;
-        }
-        else return AccessController.doPrivileged((PrivilegedAction<T>) () -> {
-            val.setAccessible(true);
-            return val;
-        });
+        return Native.access(val);
     }
     public static <T>Constructor<T> constructor(Class<T> tClass, Class<?>... args) {
         try { return access(tClass.getDeclaredConstructor(args)); }
