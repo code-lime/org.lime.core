52ba7e7e1a1e0e6b86907561d1b3e4c6a4a8f1dd
code-lime <demn2001@gmail.com>
2025-03-25 15:18:55 +0300

    extract to `LambdaInfo`

--- src/main/java/patch/Native.java
+++ src/main/java/patch/Native.java
@@ -6,7 +6,7 @@ import io.papermc.paper.util.MappingEnvironment;
 import net.neoforged.srgutils.IMappingFile;
 import org.jetbrains.annotations.NotNull;
 import org.lime.json.builder.Json;
-import org.lime.system.tuple.*;
+import org.lime.reflection.LambdaInfo;
 import org.lime.system.execute.*;
 import org.objectweb.asm.*;
 import org.objectweb.asm.Type;
@@ -15,7 +15,6 @@ import javax.annotation.Nullable;
 import java.io.*;
 import java.lang.invoke.MethodHandleInfo;
 import java.lang.invoke.SerializedLambda;
-import java.lang.reflect.*;
 import java.security.MessageDigest;
 import java.util.*;
 
@@ -56,7 +55,7 @@ public class Native {
     }
 
     public static void writeMethod(ICallable callable, Action5<Integer, String, String, String, Boolean> method) {
-        SerializedLambda lambda = infoFromLambda(callable);
+        SerializedLambda lambda = LambdaInfo.infoFromLambda(callable);
         String kind = MethodHandleInfo.referenceKindToString(lambda.getImplMethodKind());
         int opcode = switch (kind) {
             case "invokeVirtual" -> Opcodes.INVOKEVIRTUAL;
@@ -72,12 +71,12 @@ public class Native {
         method.invoke(opcode, lambda.getImplClass(), lambda.getImplMethodName(), lambda.getImplMethodSignature(), isInterface);
     }
     public static void writeField(int opcode, ICallable callable, Action4<Integer, String, String, String> field) {
-        FieldInfo info = infoFromField(callable);
+        LambdaInfo.MemberInfo info = LambdaInfo.infoFromFieldLambda(callable);
         field.invoke(opcode, info.owner(), info.name(), info.descriptor());
     }
 
     public static <T>T getMethod(ICallable callable, Func5<Integer, String, String, String, Boolean, T> method) {
-        SerializedLambda lambda = infoFromLambda(callable);
+        SerializedLambda lambda = LambdaInfo.infoFromLambda(callable);
         String kind = MethodHandleInfo.referenceKindToString(lambda.getImplMethodKind());
         int opcode = switch (kind) {
             case "invokeVirtual" -> Opcodes.INVOKEVIRTUAL;
@@ -93,86 +92,24 @@ public class Native {
         return method.invoke(opcode, lambda.getImplClass(), lambda.getImplMethodName(), lambda.getImplMethodSignature(), isInterface);
     }
     public static <T>T getField(int opcode, ICallable callable, Func4<Integer, String, String, String, T> field) {
-        FieldInfo info = infoFromField(callable);
+        LambdaInfo.MemberInfo info = LambdaInfo.infoFromFieldLambda(callable);
         return field.invoke(opcode, info.owner(), info.name(), info.descriptor());
     }
 
-    public static Method getMethod(String owner, String name, String descriptor) {
-        try {
-            Class<?> _owner = Class.forName(owner.replace('/', '.'));
-            for (Method method : _owner.getMethods()) {
-                if (method.getName().equals(name) && Type.getMethodDescriptor(method).equals(descriptor))
-                    return method;
-            }
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
-        throw new RuntimeException("Method " + owner + "." + name + descriptor + " not founded");
-    }
-    public static Method getMethod(SerializedLambda lambda) {
-        return getMethod(lambda.getImplClass(), lambda.getImplMethodName(), lambda.getImplMethodSignature());
-    }
-
     public static boolean isMethod(ICallable callable, String owner, String name, String descriptor) {
-        SerializedLambda lambda = infoFromLambda(callable);
-        if (!lambda.getImplMethodName().equals(name) || !Type.getType(lambda.getImplMethodSignature()).equals(Type.getType(descriptor))) return false;
-        if (lambda.getImplClass().equals(owner)) return true;
-        return getMethod(owner, name, descriptor).equals(getMethod(lambda));
+        LambdaInfo.MemberInfo method = LambdaInfo.MemberInfo.of(LambdaInfo.infoFromLambda(callable));
+        if (!method.name().equals(name)
+                || !Type.getType(method.descriptor()).equals(Type.getType(descriptor)))
+            return false;
+        if (method.owner().equals(owner))
+            return true;
+        return LambdaInfo.getMethod(owner, name, descriptor).equals(LambdaInfo.getMethod(method));
     }
     public static boolean isField(ICallable callable, String owner, String name, String descriptor) {
-        FieldInfo field = infoFromField(callable);
+        LambdaInfo.MemberInfo field = LambdaInfo.infoFromFieldLambda(callable);
         return field.owner().equals(owner)
                 && field.name().equals(name)
                 && Type.getType(field.descriptor()).equals(Type.getType(descriptor));
-    }
-
-    public static SerializedLambda infoFromLambda(Serializable lambda) {
-        try {
-            Method m = lambda.getClass().getDeclaredMethod("writeReplace");
-            m.setAccessible(true);
-            return (SerializedLambda)m.invoke(lambda);
-        } catch(ReflectiveOperationException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
-    public record FieldInfo(String owner, String name, String descriptor) { }
-    public static FieldInfo infoFromField(ICallable callable) {
-        try {
-            SerializedLambda lambda = infoFromLambda(callable);
-            String classFile = lambda.getImplClass();
-            String methodOwner = classFile.replace('/', '.');
-            Class<?> tClass = Class.forName(methodOwner);
-
-            String methodName = lambda.getImplMethodName();
-            String methodDescriptor = lambda.getImplMethodSignature();
-
-            //Native.log("Lambda method: " + methodOwner + "." + methodName + methodDescriptor);
-            try (InputStream stream = Objects.requireNonNull(tClass.getClassLoader().getResourceAsStream(classFile + ".class"))) {
-                ClassReader cr = new ClassReader(stream);
-                ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
-                Tuple1<FieldInfo> fieldInfo = Tuple.of(null);
-                cr.accept(new ClassVisitor(Opcodes.ASM9, cw) {
-                    @Override public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
-                        //Native.log("Compare method with: " + methodOwner.replace('/', '.') + "." + name + descriptor);
-                        if (name.equals(methodName) && descriptor.equals(methodDescriptor)) {
-                            //Native.log("Read method fileds");
-                            return new MethodVisitor(Opcodes.ASM9, super.visitMethod(access, name, descriptor, signature, exceptions)) {
-                                @Override public void visitFieldInsn(int opcode, String owner, String name, String descriptor) {
-                                    fieldInfo.val0 = new FieldInfo(owner, name, descriptor);
-                                    //Native.log("Field: " + fieldInfo.val0);
-                                    super.visitFieldInsn(opcode, owner, name, descriptor);
-                                }
-                            };
-                        }
-                        return super.visitMethod(access, name, descriptor, signature, exceptions);
-                    }
-                }, 0);
-                //Native.log(" - Field: " + fieldInfo.val0);
-                return Objects.requireNonNull(fieldInfo.val0);
-            }
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
     }
 
     private record MappingInfo(String name, String description, boolean isMethod) { }
