061569add41f798b41e71da7270cac96de5c6cf1
code-lime <demn2001@gmail.com>
2025-06-12 22:17:02 +0300

    use templates

---
+++ velocity/src/main/templates/org.lime.core.velocity/BuildConstants.java
@@ -0,0 +1,7 @@
+package org.lime.core.velocity;
+
+public class BuildConstants {
+    public static final String ID = "${pluginId}";
+    public static final String NAME = "${pluginName}";
+    public static final String VERSION = "${version}";
+}
