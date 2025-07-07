3e16ef4185627426126dcd64374b9a8c1a6bf18a
code-lime <demn2001@gmail.com>
2025-07-07 21:28:23 +0300

    fix fabric commands suggests

--- fabric/src/main/java/org/lime/core/fabric/brigadier/mixin/ArgumentTypeInfosMixin.java
+++ fabric/src/main/java/org/lime/core/fabric/brigadier/mixin/ArgumentTypeInfosMixin.java
@@ -1,22 +1,27 @@
 package org.lime.core.fabric.brigadier.mixin;
 
 import com.mojang.brigadier.arguments.ArgumentType;
+import net.minecraft.commands.synchronization.ArgumentTypeInfo;
 import net.minecraft.commands.synchronization.ArgumentTypeInfos;
 import org.lime.core.fabric.brigadier.CustomArgumentType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ArgumentTypeInfos.class)
 public abstract class ArgumentTypeInfosMixin {
-    @ModifyVariable(
+    @Inject(
             method = "unpack",
             at = @At("HEAD"),
-            index = 1)
-    private static ArgumentType<?> modify(ArgumentType<?> argumentType) {
-        return argumentType instanceof CustomArgumentType<?,?> customArgumentType
-                ? customArgumentType.mappedArgument().nativeType()
-                : argumentType;
+            cancellable = true
+    )
+    @SuppressWarnings("unchecked")
+    private static <A extends ArgumentType<?>>void unpack(A argumentType, CallbackInfoReturnable<ArgumentTypeInfo.Template<A>> cir) {
+        if (argumentType instanceof CustomArgumentType<?,?> customArgumentType) {
+            ArgumentTypeInfo.Template template = ArgumentTypeInfos.unpack(customArgumentType.mappedArgument().nativeType());
+            cir.setReturnValue(template);
+        }
     }
 }
 
