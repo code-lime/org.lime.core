c7544ca81d776be0e9eb8ac1980abadcb03ce3ee
code-lime <demn2001@gmail.com>
2025-09-06 04:20:27 +0300

    convert to use `NativeCommandConsumer`

--- common/src/main/java/org/lime/core/common/services/UpdateConfigService.java
+++ common/src/main/java/org/lime/core/common/services/UpdateConfigService.java
@@ -1,47 +1,41 @@
 package org.lime.core.common.services;
 
 import com.google.inject.Inject;
-import dev.rollczi.litecommands.argument.Argument;
-import dev.rollczi.litecommands.argument.ArgumentKey;
-import dev.rollczi.litecommands.argument.SimpleArgument;
-import dev.rollczi.litecommands.argument.resolver.collector.VarargsProfile;
-import dev.rollczi.litecommands.programmatic.LiteCommand;
-import dev.rollczi.litecommands.reflect.type.TypeToken;
-import dev.rollczi.litecommands.suggestion.Suggestion;
-import dev.rollczi.litecommands.suggestion.SuggestionResult;
+import com.mojang.brigadier.Command;
 import org.lime.core.common.Artifact;
 import org.lime.core.common.BaseInstance;
 import org.lime.core.common.api.RequireCommand;
 import org.lime.core.common.api.Service;
 import org.lime.core.common.api.commands.CommandConsumer;
-import org.lime.core.common.api.commands.LiteCommandConsumer;
+import org.lime.core.common.api.commands.NativeCommandConsumer;
+import org.slf4j.Logger;
 
 import java.util.List;
 
 public class UpdateConfigService
         implements Service {
     @Inject BaseInstance<?> instance;
-    @Inject LiteCommandConsumer.Factory<?,?,?> commandsFactory;
+    @Inject NativeCommandConsumer.Factory<?, ?> commandsFactory;
+    @Inject CustomArgumentUtility argumentsFactory;
+    @Inject Logger logger;
 
-    @RequireCommand
-    CommandConsumer<?> command() {
-        SimpleArgument<String[]> configArgument = new SimpleArgument<>("configs", TypeToken.of(String[].class));
-        configArgument.setKey(ArgumentKey.of("update.config#configs"));
-        configArgument.addProfile(new VarargsProfile(TypeToken.of(String.class), " "));
-
+    private <Sender>CommandConsumer<?> command(
+            NativeCommandConsumer.Factory<Sender, ?> commandsFactory) {
         Artifact artifact = instance.artifact();
         String suffix = artifact.proxy ? "." + artifact.key : "";
 
-        return commandsFactory.ofDynamic(new LiteCommand<>("update.config" + suffix)
-                .permissions("update.config" + suffix)
-                .literal(instance.name())
-                .argument(configArgument)
-                .execute(ctx -> {
-                    String[] configs = ctx.argument(configArgument.getName(), configArgument.getType().getRawType());
-                    instance.module.updateConfigs(List.of(configs));
-                }))
-                .with(v -> v.argumentSuggester(
-                        String[].class, configArgument.getKey(),
-                        (invocation, argument, context) -> SuggestionResult.of(instance.module.configKeys())));
+        return commandsFactory.of("update.config" + suffix, j -> j
+                .requires(commandsFactory.operator())
+                .then(commandsFactory.literal(instance.id())
+                        .then(commandsFactory.argument("config", argumentsFactory.builderString(commandsFactory.senderClass(), instance.module.configKeys()).build())
+                                .executes(ctx -> {
+                                    String config = ctx.getArgument("config", String.class);
+                                    instance.module.updateConfigs(List.of(config));
+                                    return Command.SINGLE_SUCCESS;
+                                }))));
+    }
+    @RequireCommand
+    CommandConsumer<?> command() {
+        return command(commandsFactory);
     }
 }
