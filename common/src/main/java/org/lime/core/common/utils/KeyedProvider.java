27aa78a2cf6d63ecc3e0a74038a8b845d57aaa46
code-lime <demn2001@gmail.com>
2025-09-06 04:20:35 +0300

    add `KeyedProvider`

---
+++ common/src/main/java/org/lime/core/common/utils/KeyedProvider.java
@@ -0,0 +1,36 @@
+package org.lime.core.common.utils;
+
+import org.lime.core.common.utils.system.Lock;
+import org.lime.core.common.utils.system.execute.Func1;
+
+import java.time.Duration;
+
+public class KeyedProvider<K, V> {
+    private final Lock lock = Lock.create();
+
+    private K key;
+    private V value;
+
+    private final Func1<K, V> factory;
+
+    public KeyedProvider(K initialKey, Func1<K, V> factory) {
+        this.factory = factory;
+        this.key = initialKey;
+        this.value = factory.apply(initialKey);
+    }
+
+    public V get(K key) {
+        try (var v = lock.lock()) {
+            if (!this.key.equals(key)) {
+                this.key = key;
+                this.value = factory.apply(key);
+            }
+            return this.value;
+        }
+    }
+
+    public static <K, V>KeyedProvider<K, V> of(K initialKey, Func1<K, V> factory) {
+        return new KeyedProvider<>(initialKey, factory);
+    }
+}
+
