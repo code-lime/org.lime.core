9ac600e1db7169f9c2d74d2c48ccb1829ce674b5
code-lime <demn2001@gmail.com>
2025-09-06 04:20:40 +0300

    add `WaitItem`

---
+++ common/src/main/java/org/lime/core/common/utils/WaitItem.java
@@ -0,0 +1,35 @@
+package org.lime.core.common.utils;
+
+import org.lime.core.common.utils.system.execute.Func1;
+
+import java.time.Duration;
+
+public record WaitItem<T>(T item, long time) {
+    public boolean isDone() {
+        return isDone(System.currentTimeMillis());
+    }
+    public boolean isDone(long now) {
+        return time < now;
+    }
+
+    public int waitSeconds() {
+        return waitSeconds(System.currentTimeMillis());
+    }
+    public int waitSeconds(long now) {
+        return (int) Math.max(0, time - now);
+    }
+
+    public WaitItem<T> change(T otherItem) {
+        return new WaitItem<>(otherItem, time);
+    }
+    public <J> WaitItem<J> map(Func1<T, J> mapItem) {
+        return new WaitItem<>(mapItem.invoke(item), time);
+    }
+
+    public static <T> WaitItem<T> of(T item, Duration delay) {
+        return of(item, System.currentTimeMillis(), delay);
+    }
+    public static <T> WaitItem<T> of(T item, long now, Duration delay) {
+        return new WaitItem<>(item, now + delay.toMillis());
+    }
+}
