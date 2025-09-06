fe9c6b7acb57ee774a3ee8759f8998c0269b324a
code-lime <demn2001@gmail.com>
2025-09-06 04:19:22 +0300

    add `DurationUtils`

---
+++ common/src/main/java/org/lime/core/common/utils/adapters/DurationUtils.java
@@ -0,0 +1,54 @@
+package org.lime.core.common.utils.adapters;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+
+public class DurationUtils {
+    private static void addPart(StringBuilder builder, long value, String suffix) {
+        if (value <= 0)
+            return;
+        builder.append(value).append(suffix);
+    }
+    public static String write(Duration value) {
+        if (value.isNegative())
+            return "0s";
+
+        StringBuilder builder = new StringBuilder();
+
+        addPart(builder, value.toSecondsPart(), "s");
+        addPart(builder, value.toMinutesPart(), "m");
+        addPart(builder, value.toHoursPart(), "h");
+        addPart(builder, value.toDays(), "d");
+
+        if (builder.isEmpty())
+            builder.append("0s");
+
+        return builder.toString();
+    }
+    public static Duration read(String value) {
+        int amount = 0;
+        Duration duration = Duration.ZERO;
+
+        for (char ch : value.toCharArray()) {
+            if ('0' <= ch && ch <= '9') {
+                amount = amount * 10 + (ch - '0');
+            } else {
+                TemporalUnit unit = switch (ch) {
+                    case 's' -> ChronoUnit.SECONDS;
+                    case 'm' -> ChronoUnit.MINUTES;
+                    case 'h' -> ChronoUnit.HOURS;
+                    case 'd' -> ChronoUnit.DAYS;
+                    default -> throw new IllegalStateException("Unexpected character: " + ch);
+                };
+                duration = duration.plus(amount, unit);
+                amount = 0;
+            }
+        }
+
+        if (amount > 0)
+            duration = duration.plusSeconds(amount);
+
+        return duration;
+    }
+}
