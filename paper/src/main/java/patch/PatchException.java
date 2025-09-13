de34f3c18a3a4f698e9476b909cf9e9661c069cb
code-lime <demn2001@gmail.com>
2025-09-13 04:45:43 +0300

    add `additional` information

--- paper/src/main/java/patch/PatchException.java
+++ paper/src/main/java/patch/PatchException.java
@@ -1,10 +1,28 @@
 package patch;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class PatchException extends RuntimeException {
+    public final List<String> additional = new ArrayList<>();
+
     public PatchException(String message, JarArchiveBase jar, String className) {
         super(message + " (" + jar.name() + "::" + className + ")");
     }
     public PatchException(String message, JarArchiveBase jar, MethodInfo method) {
         super(message + " (" + jar.name() + "::" + method.toInfo() + ")");
+    }
+
+    @Override
+    public String getMessage() {
+        if (additional.isEmpty())
+            return super.getMessage();
+        return super.getMessage() + "\n" + String.join("\n", additional);
+    }
+
+    public PatchException addAdditional(Collection<String> additional) {
+        this.additional.addAll(additional);
+        return this;
     }
 }
