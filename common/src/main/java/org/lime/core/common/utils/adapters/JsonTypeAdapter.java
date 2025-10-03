b0c478abb894d3b9a20e6a638ae311fed894942b
code-lime <demn2001@gmail.com>
2025-10-03 19:01:01 +0300

    provider of gson support

--- common/src/main/java/org/lime/core/common/utils/adapters/JsonTypeAdapter.java
+++ common/src/main/java/org/lime/core/common/utils/adapters/JsonTypeAdapter.java
@@ -5,17 +5,20 @@ import com.google.gson.JsonElement;
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
+import com.google.inject.Provider;
+import org.lime.core.common.utils.Lazy;
 
 import java.io.IOException;
 
 public abstract class JsonTypeAdapter<T, E extends JsonElement>
         extends TypeAdapter<T> {
-    public final Gson gson;
-    public final TypeAdapter<E> elementTypeAdapter;
+    public final Lazy<TypeAdapter<E>> elementTypeAdapter;
 
     public JsonTypeAdapter(Gson gson, Class<E> jsonElementClass) {
-        this.gson = gson;
-        this.elementTypeAdapter = gson.getAdapter(jsonElementClass);
+        this.elementTypeAdapter = Lazy.of(gson.getAdapter(jsonElementClass));
+    }
+    public JsonTypeAdapter(Provider<Gson> gson, Class<E> jsonElementClass) {
+        this.elementTypeAdapter = Lazy.of(() -> gson.get().getAdapter(jsonElementClass));
     }
 
     public abstract E write(T value) throws IOException;
@@ -23,10 +26,10 @@ public abstract class JsonTypeAdapter<T, E extends JsonElement>
 
     @Override
     public void write(JsonWriter out, T value) throws IOException {
-        elementTypeAdapter.write(out, write(value));
+        elementTypeAdapter.value().write(out, write(value));
     }
     @Override
     public T read(JsonReader in) throws IOException {
-        return read(elementTypeAdapter.read(in));
+        return read(elementTypeAdapter.value().read(in));
     }
 }
