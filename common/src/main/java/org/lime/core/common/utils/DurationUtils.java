9254071951f27b8235f99bfa879d0b39cb989b43
code-lime <demn2001@gmail.com>
2025-10-19 20:05:38 +0300

    refactor `DurationUtils`

--- common/src/main/java/org/lime/core/common/utils/DurationUtils.java
+++ common/src/main/java/org/lime/core/common/utils/DurationUtils.java
@@ -1,10 +1,64 @@
 package org.lime.core.common.utils;
 
+import net.kyori.adventure.util.Ticks;
+
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
 import java.time.temporal.TemporalUnit;
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DurationUtils {
+    public static final TemporalUnit TICKS_UNIT = new TemporalUnit() {
+        private final long milliseconds = Ticks.SINGLE_TICK_DURATION_MS;
+        private final Duration duration = Duration.ofMillis(this.milliseconds);
+        @Override
+        public Duration getDuration() {
+            return duration;
+        }
+        @Override
+        public boolean isDurationEstimated() {
+            return false;
+        }
+        @Override
+        public boolean isDateBased() {
+            return false;
+        }
+        @Override
+        public boolean isTimeBased() {
+            return true;
+        }
+        @SuppressWarnings("unchecked")
+        @Override
+        public <R extends Temporal> R addTo(final R temporal, final long amount) {
+            return (R) temporal.plus(this.getDuration().multipliedBy(amount));
+        }
+        @Override
+        public long between(final Temporal start, final Temporal end) {
+            return start.until(end, ChronoUnit.MILLIS) / this.milliseconds;
+        }
+
+        @Override
+        public String toString() {
+            return "Ticks";
+        }
+    };
+    public record UnitSymbol(char symbol, TemporalUnit unit) {
+        public static UnitSymbol of(char symbol, TemporalUnit unit) {
+            return new UnitSymbol(symbol, unit);
+        }
+    }
+    public static LinkedHashMap<Character, UnitSymbol> UNITS = Stream.of(
+            UnitSymbol.of('w', ChronoUnit.WEEKS),
+            UnitSymbol.of('d', ChronoUnit.DAYS),
+            UnitSymbol.of('h', ChronoUnit.HOURS),
+            UnitSymbol.of('m', ChronoUnit.MINUTES),
+            UnitSymbol.of('s', ChronoUnit.SECONDS),
+            UnitSymbol.of('t', TICKS_UNIT)
+    ).collect(Collectors.toMap(UnitSymbol::symbol, v -> v, (x, y) -> y, LinkedHashMap::new));
+
     public static final int MILLIS_PER_TICK = 50;
 
     private static void addPart(StringBuilder builder, long value, String suffix) {
@@ -16,13 +70,25 @@ public class DurationUtils {
         if (value.isNegative())
             return "0s";
 
+        Duration remaining = value;
+
         StringBuilder builder = new StringBuilder();
 
-        addPart(builder, value.toMillisPart() / MILLIS_PER_TICK, "t");
-        addPart(builder, value.toSecondsPart(), "s");
-        addPart(builder, value.toMinutesPart(), "m");
-        addPart(builder, value.toHoursPart(), "h");
-        addPart(builder, value.toDays(), "d");
+        for (UnitSymbol us : UNITS.values()) {
+            char symbol = us.symbol();
+            TemporalUnit unit = us.unit();
+
+            Duration unitDuration = unit.getDuration();
+            if (unitDuration.isZero() || unitDuration.isNegative())
+                continue;
+
+            long amount = remaining.dividedBy(unitDuration);
+
+            if (amount > 0) {
+                addPart(builder, amount, String.valueOf(symbol));
+                remaining = remaining.minus(unitDuration.multipliedBy(amount));
+            }
+        }
 
         if (builder.isEmpty())
             builder.append("0s");
@@ -33,30 +99,37 @@ public class DurationUtils {
         long amount = 0;
         Duration duration = Duration.ZERO;
 
-        for (char ch : value.toCharArray()) {
-            if ('0' <= ch && ch <= '9') {
+        for (int i = 0; i < value.length(); i++) {
+            char ch = value.charAt(i);
+            if (Character.isDigit(ch)) {
                 amount = amount * 10 + (ch - '0');
-            } else {
-                if (ch == 't') {
-                    duration = duration.plus(amount * MILLIS_PER_TICK, ChronoUnit.MILLIS);
-                } else {
-                    TemporalUnit unit = switch (ch) {
-                        case 's' -> ChronoUnit.SECONDS;
-                        case 'm' -> ChronoUnit.MINUTES;
-                        case 'h' -> ChronoUnit.HOURS;
-                        case 'd' -> ChronoUnit.DAYS;
-                        default -> throw new IllegalStateException("Unexpected character: " + ch);
-                    };
-                    duration = duration.plus(amount, unit);
-                }
-                amount = 0;
+                continue;
             }
+            if (amount <= 0)
+                continue;
+            UnitSymbol us = UNITS.get(ch);
+            if (us == null)
+                throw new IllegalStateException("Unexpected character: " + ch);
+            var unit = us.unit();
+            if (unit.isDurationEstimated())
+                duration = duration.plus(unit.getDuration().multipliedBy(amount));
+            else
+                duration = duration.plus(amount, unit);
+            amount = 0;
         }
 
         if (amount > 0)
             duration = duration.plusSeconds(amount);
 
         return duration;
+    }
+    public static Stream<String> readVariants(String value) {
+        if (!value.isEmpty()) {
+            char ch = value.charAt(value.length() - 1);
+            if (Character.isDigit(ch))
+                return UNITS.keySet().stream().map(v -> value + v);
+        }
+        return UNITS.keySet().stream().map(v -> value + "1" + v);
     }
 
     public static Duration ofTicks(long ticks) {
