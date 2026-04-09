343114503a556e1e2c71b0e926c254b6d716df23
code-lime <demn2001@gmail.com>
2026-04-09 23:17:00 +0300

    add `ArgumentReader`.`map`

--- common/src/main/java/org/lime/core/common/api/commands/brigadier/arguments/ArgumentReader.java
+++ common/src/main/java/org/lime/core/common/api/commands/brigadier/arguments/ArgumentReader.java
@@ -2,11 +2,15 @@ package org.lime.core.common.api.commands.brigadier.arguments;
 
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import org.lime.core.common.utils.execute.Func1;
 
 public interface ArgumentReader<T, S> {
     T read(CommandContext<S> context, String parameter) throws CommandSyntaxException;
 
     default ArgumentAccess<T> access(CommandContext<S> context, String parameter) {
         return () -> read(context, parameter);
+    }
+    default <I>ArgumentReader<I, S> map(Func1<T, I> convert) {
+        return (ctx, parameter) -> convert.invoke(read(ctx, parameter));
     }
 }
