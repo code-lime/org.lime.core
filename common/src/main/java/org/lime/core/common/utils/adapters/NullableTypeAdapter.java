eabe2d5fcc474c7f2d24d9f91dedd724ef8f2e9b
code-lime <demn2001@gmail.com>
2026-04-09 23:30:29 +0300

    add `NullableTypeAdapter`

---
+++ common/src/main/java/org/lime/core/common/utils/adapters/NullableTypeAdapter.java
@@ -0,0 +1,38 @@
+package org.lime.core.common.utils.adapters;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+
+public final class NullableTypeAdapter<T>
+        extends TypeAdapter<T> {
+    private final TypeAdapter<T> adapter;
+
+    public NullableTypeAdapter(TypeAdapter<T> adapter) {
+        this.adapter = adapter;
+    }
+
+    @Override
+    public void write(JsonWriter out, T value) throws IOException {
+        if (value == null) {
+            out.nullValue();
+            return;
+        }
+        adapter.write(out, value);
+    }
+    @Override
+    public T read(JsonReader in) throws IOException {
+        if (in.peek() == JsonToken.NULL) {
+            in.nextNull();
+            return null;
+        }
+        return adapter.read(in);
+    }
+
+    public static <T>NullableTypeAdapter<T> of(TypeAdapter<T> adapter) {
+        return new NullableTypeAdapter<>(adapter);
+    }
+}
