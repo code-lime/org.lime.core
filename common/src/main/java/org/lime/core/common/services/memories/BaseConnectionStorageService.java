edc3f9fbde98bf73a659f6f3eafafcf1d56a051d
code-lime <demn2001@gmail.com>
2026-05-05 01:58:39 +0300

    add `getOrCreate` & `modifyEvery` & `every`

--- common/src/main/java/org/lime/core/common/services/memories/BaseConnectionStorageService.java
+++ common/src/main/java/org/lime/core/common/services/memories/BaseConnectionStorageService.java
@@ -6,12 +6,15 @@ import org.jetbrains.annotations.Nullable;
 import org.lime.core.common.api.Service;
 import org.lime.core.common.services.ScheduleTaskService;
 import org.lime.core.common.utils.Disposable;
+import org.lime.core.common.utils.execute.Action2;
 import org.lime.core.common.utils.execute.Func1;
+import org.lime.core.common.utils.execute.Func2;
 
 import java.time.Duration;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,6 +25,9 @@ public abstract class BaseConnectionStorageService
         public <T> Optional<T> get(MemoryKey<T> key) {
             return Optional.ofNullable(memory.get(key))
                     .map(key::castOrNull);
+        }
+        public <T> Optional<T> getOrCreate(MemoryKey<T> key, Supplier<T> supplier) {
+            return key.cast(memory.computeIfAbsent(key, v -> supplier.get()));
         }
         public <T> void set(MemoryKey<T> key, @Nullable T value) {
             if (value == null) memory.remove(key);
@@ -76,10 +82,13 @@ public abstract class BaseConnectionStorageService
             public MemoryKey<T> key() {
                 return key;
             }
-
             @Override
             public Optional<T> get(UUID playerId) {
                 return BaseConnectionStorageService.this.get(key, playerId);
+            }
+            @Override
+            public Optional<T> getOrCreate(UUID playerId, Supplier<T> supplier) {
+                return BaseConnectionStorageService.this.getOrCreate(key, playerId, supplier);
             }
             @Override
             public void set(UUID playerId, @Nullable T value) {
@@ -96,6 +105,14 @@ public abstract class BaseConnectionStorageService
             @Override
             public void modify(UUID playerId, Func1<@Nullable T, @Nullable T> modify) {
                 BaseConnectionStorageService.this.modify(key, playerId, modify);
+            }
+            @Override
+            public void modifyEvery(Func2<UUID, T, T> modify) {
+                BaseConnectionStorageService.this.modifyEvery(key, modify);
+            }
+            @Override
+            public void every(Action2<UUID, T> action) {
+                BaseConnectionStorageService.this.every(key, action);
             }
         };
     }
@@ -115,6 +132,9 @@ public abstract class BaseConnectionStorageService
     public <T> Optional<T> get(MemoryKey<T> key, UUID playerId) {
         return Optional.ofNullable(memories.get(playerId)).flatMap(v -> v.get(key));
     }
+    private <T> Optional<T> getOrCreate(MemoryKey<T> key, UUID playerId, Supplier<T> supplier) {
+        return Optional.ofNullable(memories.get(playerId)).flatMap(v -> v.getOrCreate(key, supplier));
+    }
     public <T> void set(MemoryKey<T> key, UUID playerId, @Nullable T value) {
         Optional.ofNullable(memories.get(playerId)).ifPresent(v -> v.set(key, value));
     }
@@ -126,5 +146,11 @@ public abstract class BaseConnectionStorageService
     }
     public <T> void modify(MemoryKey<T> key, UUID playerId, Func1<@Nullable T, @Nullable T> modify) {
         Optional.ofNullable(memories.get(playerId)).ifPresent(v -> v.modify(key, modify));
+    }
+    private <T> void modifyEvery(MemoryKey<T> key, Func2<UUID, T, T> modify) {
+        memories.forEach((playerId, memory) -> memory.modify(key, v -> modify.invoke(playerId, v)));
+    }
+    private <T> void every(MemoryKey<T> key, Action2<UUID, T> action) {
+        memories.forEach((playerId, memory) -> memory.get(key).ifPresent(v -> action.invoke(playerId, v)));
     }
 }
--- common/src/main/java/org/lime/core/common/services/memories/MemoryStorage.java
+++ common/src/main/java/org/lime/core/common/services/memories/MemoryStorage.java
@@ -1,17 +1,26 @@
 package org.lime.core.common.services.memories;
 
 import org.jetbrains.annotations.Nullable;
+import org.lime.core.common.utils.execute.Action2;
 import org.lime.core.common.utils.execute.Func1;
+import org.lime.core.common.utils.execute.Func2;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.function.Supplier;
 
 public interface MemoryStorage<T> {
     MemoryKey<T> key();
 
     Optional<T> get(UUID playerId);
+    Optional<T> getOrCreate(UUID playerId, Supplier<T> supplier);
+
     void set(UUID playerId, @Nullable T value);
     boolean has(UUID playerId);
     boolean remove(UUID playerId);
+
     void modify(UUID playerId, Func1<@Nullable T, @Nullable T> modify);
+    void modifyEvery(Func2<UUID, T, @Nullable T> modify);
+
+    void every(Action2<UUID, T> action);
 }
