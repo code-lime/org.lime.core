c2ad2d95b99e331cb121c1af74e68243966c754f
code-lime <demn2001@gmail.com>
2026-07-08 04:21:01 +0300

    fix `RequiredArgumentBuilder.type` access

--- fabric/src/main/java/org/lime/core/fabric/commands/brigadier/CommandHelperUtils.java
+++ fabric/src/main/java/org/lime/core/fabric/commands/brigadier/CommandHelperUtils.java
@@ -1,16 +1,23 @@
 package org.lime.core.fabric.commands.brigadier;
 
+import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.tree.CommandNode;
-import net.kyori.adventure.platform.fabric.impl.accessor.brigadier.builder.RequiredArgumentBuilderAccess;
+import org.lime.core.common.reflection.ReflectionField;
+import org.lime.core.common.utils.execute.Action2;
 
 public class CommandHelperUtils {
+    @SuppressWarnings("unchecked")
+    private static final Action2<RequiredArgumentBuilder<?,?>, ArgumentType<?>> typeSetter = ReflectionField.of(RequiredArgumentBuilder.class, "type")
+            .nonFinal()
+            .setter(Action2.class);
+
     public static <T> CommandNode<T> argumentBuilder(
             ArgumentBuilder<T, ?> argumentBuilder) {
         if (argumentBuilder instanceof RequiredArgumentBuilder<?,?> builder) {
             if (builder.getType() instanceof CustomArgumentType<?,?> customArgumentType) {
-                ((RequiredArgumentBuilderAccess) builder).accessor$type(customArgumentType.mappedArgument().nativeType());
+                typeSetter.invoke(builder, customArgumentType.mappedArgument().nativeType());
                 if (builder.getSuggestionsProvider() == null)
                     builder.suggests(customArgumentType::listSuggestions);
             }
